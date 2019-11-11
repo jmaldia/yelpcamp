b8705417c45deb25afc7625587b99810f5a10baf
Jon Maldia <jon@maldia.net>
2019-11-10 19:12:29 -0500

    Update to Bootstrap 4

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ let bodyParser = require("body-parser");
 let app = express();
 
 let campgrounds = [
-    { name: "Salmon Creek", image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false" },
-    { name: "Ganite Hill", image: "https://www.nps.gov/mora/planyourvisit/images/OhanaCampground2016_CMeleedy_01_web.jpeg?maxwidth=1200&maxheight=1200&autorotate=false" }, 
-    { name: "Mountain Goat's Rest", image:"https://haulihuvila.com/wp-content/uploads/2012/09/hauli-huvila-campgrounds-lg.jpg" }
+    { name: "Salmon Creek", image: "https://d2s0f1q6r2lxto.cloudfront.net/pub/ProTips/wp-content/uploads/2017/04/how-to-set-up-a-campsite.jpg" },
+    { name: "Ganite Hill", image: "https://d2s0f1q6r2lxto.cloudfront.net/pub/ProTips/wp-content/uploads/2015/09/camp-checklist.jpg" }, 
+    { name: "Mountain Goat's Rest", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxpL78CTsOJLLaVOdA3Ftktb_iQ-0CSHHmn4omFZx1RZ3yd1so&s" },
+    { name: "Salmon Creek", image: "https://d2s0f1q6r2lxto.cloudfront.net/pub/ProTips/wp-content/uploads/2017/04/how-to-set-up-a-campsite.jpg" },
+    { name: "Ganite Hill", image: "https://d2s0f1q6r2lxto.cloudfront.net/pub/ProTips/wp-content/uploads/2015/09/camp-checklist.jpg" }, 
+    { name: "Mountain Goat's Rest", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxpL78CTsOJLLaVOdA3Ftktb_iQ-0CSHHmn4omFZx1RZ3yd1so&s" }
 ]
 
 app.set("view engine", "ejs");
