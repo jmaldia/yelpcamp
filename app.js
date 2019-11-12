460a90d93bc57927863a3ad668e268cad170cd75
Jon Maldia <jon@maldia.net>
2019-11-12 11:44:50 -0500

    Migrate from YelpCamp to Rutherford Spots

--- app.js
+++ app.js
@@ -4,60 +4,70 @@ let mongoose    = require("mongoose");
 // run server
 let app = express();
 // connect to mongo db
-mongoose.connect("mongodb://localhost/yelp_camp");
-
-// SCHEMA SETUP
-let campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String
-});
-
-let Campground = mongoose.model("Campground", campgroundSchema);
-
-Campground.find({}, (err, campgrounds) => {
-    if (err) {
-        console.log("OH NO, ERROR!");
-    } else {
-        console.log(campgrounds);
-    }
-});
-
-Campground.create({ 
-        name: "Ganite Hill", 
-        image: "https://d2s0f1q6r2lxto.cloudfront.net/pub/ProTips/wp-content/uploads/2015/09/camp-checklist.jpg" 
-    }, (err, campground) => {
-        if (err) {
-            console.log("SOMETHING WENT WRONG!", err);
-        } else {
-            console.log("WE JUST CREATED A CAMPGROUND TO THE DB!");
-            console.log(campground);
-        }
-    });
+mongoose.connect("mongodb://localhost:27017/neighborhood_spots", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+// SCHEMA SETUP
+let spotSchema = new mongoose.Schema({
+    name: String,
+    image: String, 
+    description: String
+});
+
+let Spot = mongoose.model("Spot", spotSchema);
+
+// EXPRESS ROUTES
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", { campgrounds: campgrounds });
+app.get("/spots", (req, res) => {
+    // Get campgrounds from DB
+    Spot.find({}, (err, allSpots) => {
+        if (err) {
+            console.log("OH NO, ERROR!");
+        } else {
+            res.render("spots", { spots: allSpots });
+            console.log(allSpots);
+        }
+    });
 });
 
 // Post route to add new campground
-app.post("/campgrounds", (req, res) => {
+app.post("/spots", (req, res) => {
     let name = req.body.name;
     let image = req.body.image;
+    let description = req.body.description;
+    let newSpot = {
+        name: name, 
+        image: image,
+        description: description
+    }
 
-    campgrounds.push({ name: name, image: image });
+    // Create new campground on DB
+    Spot.create(newSpot, (err, newlyCreatedSpot) => {
+            if (err) {
+                console.log("SOMETHING WENT WRONG!", err);
+            } else {
+                console.log("WE JUST CREATED A SPOT ON THE DB!");
+                console.log(newlyCreatedSpot);
+                res.redirect("/spots");
+            }
+        });
 
-    res.redirect("/campgrounds");
+    // campgrounds.push({ name: name, image: image }); - old code
 });
 
-app.get("/campgrounds/new", (req, res) => {
+app.get("/spots/new", (req, res) => {
     res.render("new")
-})
+});
+
+app.get("/spots/:id", (res, req) => {
+
+    res.setEncoding("SHOW PAGE");
+});
 
 // Tell express to listen for requests -start server 
 // start your app with this command: PORT=3000 node app.js
