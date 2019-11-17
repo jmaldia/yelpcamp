d374cbdaa54d0baf0ecc2c6c73aa303c4521228a
Jon Maldia <jon@maldia.net>
2019-11-17 13:00:57 -0500

    Refactor code

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ mongoose.connect("mongodb://localhost:27017/neighborhood_spots", {useNewUrlParse
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname+"/public"));
 
 // EXPRESS ROUTES
 app.get("/", (req, res) => {
